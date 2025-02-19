0272fb8feefcd571245d2b347e028e55152167a3
Jurien Hamaker <whoami@jurien.dev>
2025-02-19 18:13:18 +0100

    feat(atoms/dropdown-menu/item): New branding implementation

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -525,6 +525,10 @@ export namespace Components {
          */
         "active": boolean;
         /**
+          * Wether the item is disabled
+         */
+        "disabled": boolean;
+        /**
           * Wether to enable the hover state
          */
         "enableHover": boolean;
@@ -2807,6 +2811,10 @@ declare namespace LocalJSX {
           * Wether the dropdown menu item is active
          */
         "active"?: boolean;
+        /**
+          * Wether the item is disabled
+         */
+        "disabled"?: boolean;
         /**
           * Wether to enable the hover state
          */
