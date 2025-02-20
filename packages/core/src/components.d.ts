3d282531b700b90a42a5d870d0f034acff67ff11
Jurien Hamaker <whoami@jurien.dev>
2025-02-20 11:34:37 +0100

    feat(organisms/navbar): New branding implementation

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -862,10 +862,6 @@ export namespace Components {
     }
     interface PNavbar {
         /**
-          * The text to display for the close button
-         */
-        "closeText": string;
-        /**
           * The text to display for the menu button & sidebar title
          */
         "menuText": string;
@@ -3189,9 +3185,6 @@ declare namespace LocalJSX {
         "showClose"?: boolean;
     }
     interface PNavbar {
-        /**
-          * The text to display for the close button
-         */
         /**
           * The text to display for the menu button & sidebar title
          */
