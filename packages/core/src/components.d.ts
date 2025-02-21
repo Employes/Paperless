fc58aa3432c932cb4ee0a4827a87f52107ac42cb
Jurien Hamaker <whoami@jurien.dev>
2025-02-21 12:39:24 +0100

    feat(molecules/profile): Use button for profile when dropdown available

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -133,6 +133,10 @@ export namespace Components {
     }
     interface PButton {
         /**
+          * Wether to force an active state
+         */
+        "active": boolean;
+        /**
           * The type of the button
          */
         "as": 'a' | 'button';
@@ -2417,6 +2421,10 @@ declare namespace LocalJSX {
     interface PBadge {
     }
     interface PButton {
+        /**
+          * Wether to force an active state
+         */
+        "active"?: boolean;
         /**
           * The type of the button
          */
