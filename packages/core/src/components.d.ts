e23a53bf321da3a2e46e3ec87d3ebbb9634fbb86
Jurien Hamaker <whoami@jurien.dev>
2025-02-21 13:34:20 +0100

    feat(molecules/button): Add dropdown variant

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -207,7 +207,11 @@ export namespace Components {
         /**
           * The variant of the button
          */
-        "variant": 'primary' | 'secondary' | 'transparent' | 'text';
+        "variant": | 'primary'
+		| 'secondary'
+		| 'transparent'
+		| 'text'
+		| 'dropdown';
     }
     interface PButtonGroup {
         /**
@@ -2500,7 +2504,11 @@ declare namespace LocalJSX {
         /**
           * The variant of the button
          */
-        "variant"?: 'primary' | 'secondary' | 'transparent' | 'text';
+        "variant"?: | 'primary'
+		| 'secondary'
+		| 'transparent'
+		| 'text'
+		| 'dropdown';
     }
     interface PButtonGroup {
         /**
