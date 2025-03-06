59a14692f22071b28e07664b769e5726146d5451
Jurien Hamaker <whoami@jurien.dev>
2025-03-06 11:43:50 +0100

    feat(atoms/dropdown-menu/item): Add negative variant

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -545,6 +545,10 @@ export namespace Components {
          */
         "active": boolean;
         /**
+          * Wether the dropdown menu item has a checkbox
+         */
+        "checkbox": boolean;
+        /**
           * Wether the item is disabled
          */
         "disabled": boolean;
@@ -563,7 +567,7 @@ export namespace Components {
         /**
           * The variant of the item
          */
-        "variant": 'default' | 'checkbox';
+        "variant": 'default' | 'negative';
     }
     interface PFloatingMenuContainer {
         /**
@@ -2884,6 +2888,10 @@ declare namespace LocalJSX {
          */
         "active"?: boolean;
         /**
+          * Wether the dropdown menu item has a checkbox
+         */
+        "checkbox"?: boolean;
+        /**
           * Wether the item is disabled
          */
         "disabled"?: boolean;
@@ -2902,7 +2910,7 @@ declare namespace LocalJSX {
         /**
           * The variant of the item
          */
-        "variant"?: 'default' | 'checkbox';
+        "variant"?: 'default' | 'negative';
     }
     interface PFloatingMenuContainer {
         /**
