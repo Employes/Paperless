144b67ee6da597557d11bf97d2e9092d07acf982
Jurien Hamaker <whoami@jurien.dev>
2025-03-13 17:22:44 +0100

    chore(angular/playground): Add kitchen sink for inputs

--- packages/angular/projects/playground/src/main.ts
+++ packages/angular/projects/playground/src/main.ts
@@ -2,9 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import { applyPolyfills, defineCustomElements } from '@paperless/core/loader';
+import { defineCustomElements } from '@paperless/core/loader';
 
-applyPolyfills()
-	.then(() => defineCustomElements())
-	.then(() => platformBrowserDynamic().bootstrapModule(AppModule))
+defineCustomElements();
+platformBrowserDynamic()
+	.bootstrapModule(AppModule)
 	.catch(err => console.error(err));
