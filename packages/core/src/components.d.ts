42ad2c893525e57a563332f723c81596d825b396
Jurien Hamaker <whoami@jurien.dev>
2025-03-17 11:38:09 +0100

    feat(molecules/navigation/item): Add loading property

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -1008,6 +1008,10 @@ export namespace Components {
          */
         "icon": IconVariant;
         /**
+          * Wether the navigation item is loading
+         */
+        "loading": boolean;
+        /**
           * The target of the navigation item
          */
         "target": string;
@@ -3910,6 +3914,10 @@ declare namespace LocalJSX {
           * Icon of the navigation item
          */
         "icon": IconVariant;
+        /**
+          * Wether the navigation item is loading
+         */
+        "loading"?: boolean;
         /**
           * The target of the navigation item
          */
