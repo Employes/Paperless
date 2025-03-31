a303ab17056b975b12430d2943c437fb24150d2f
Jurien Hamaker <whoami@jurien.dev>
2025-03-31 14:17:39 +0200

    refactor(atoms/floating-menu/container & item): Apply rebranding & apply to components that use it

--- packages/core/src/components/atoms/floating-menu-container/floating-menu-container.component.tsx
+++ packages/core/src/components/atoms/floating-menu-container/floating-menu-container.component.tsx
@@ -1,25 +1,141 @@
-import { Component, Host, Prop, h } from '@stencil/core';
+import {
+	Component,
+	Event,
+	EventEmitter,
+	Fragment,
+	Host,
+	Listen,
+	Prop,
+	h,
+	Element,
+	State,
+} from '@stencil/core';
+import { cva } from 'class-variance-authority';
+import { formatTranslation, getLocaleComponentStrings } from '../../../utils';
+
+const container = cva(
+	[
+		'floating-menu-container z-floating-menu',
+		'bg-dark-teal-600 shadow-3',
+		'h-10 py-1 w-auto',
+		'rounded-lg',
+		'text-sm',
+		'flex items-center gap-2',
+	],
+	{
+		variants: {
+			enableAmountSelected: {
+				false: 'px-1',
+				true: 'pl-3 pr-1',
+			},
+		},
+	}
+);
+
+const amountSelected = cva([
+	'text-dark-teal-100',
+	'min-w-0',
+	'overflow-hidden text-ellipsis text-nowrap',
+]);
+
+export type amountSelectedTemplateFunc = (amount: number) => string;
 
 @Component({
 	tag: 'p-floating-menu-container',
-	styleUrl: 'floating-menu-container.component.scss',
+	styleUrl: 'floating-menu-container.component.css',
 	shadow: true,
 })
 export class FloatingMenuContainer {
 	/**
-	 * Weather the container is used in the table
+	 * Wether the container is used in the table
 	 */
 	@Prop() usedInTable: boolean = false;
 
+	/**
+	 * Wether to show the amount selected
+	 */
+	@Prop() enableAmountSelected = true;
+
+	/**
+	 * The amount selected
+	 */
+	@Prop() amount = 0;
+
+	/**
+	 * Wether to enablethe close button
+	 */
+	@Prop() enableClose = true;
+
+	/**
+	 * The template for amount selected
+	 */
+	@Prop() amountSelectedTemplate: amountSelectedTemplateFunc = (
+		amount: number
+	) =>
+		formatTranslation(
+			(amount === 1
+				? this._locales.amount_selected
+				: this._locales.amount_selected_plural
+			)?.replace('{{amount}}', amount)
+		);
+
+	/**
+	 * Event whenever the close button is clicked
+	 */
+	@Event() close: EventEmitter<MouseEvent>;
+
+	/**
+	 * The host element
+	 */
+	@Element() private _el: HTMLElement;
+
+	/**
+	 * Locales used for this component
+	 */
+	@State() private _locales: any = {};
+
+	componentWillLoad() {
+		this._setLocales();
+	}
+
 	render() {
 		return (
-			<Host class="p-floating-menu-container">
-				{this.usedInTable ? (
-					<slot name="floating-menu-item" />
-				) : (
-					<slot />
-				)}
+			<Host>
+				<div
+					class={container({
+						enableAmountSelected: this.enableAmountSelected,
+					})}
+				>
+					{this.enableAmountSelected && (
+						<Fragment>
+							<p class={amountSelected()}>
+								{this.amountSelectedTemplate(this.amount)}
+							</p>
+
+							<p-divider
+								class='mx-0 h-4 text-black-teal-400'
+								variant='vertical'
+							/>
+						</Fragment>
+					)}
+
+					{this.usedInTable ? <slot name='floating-menu-item' /> : <slot />}
+
+					{this.enableClose && (
+						<div
+							class='ml-auto cursor-pointer p-2 text-dark-teal-300 hover:text-dark-teal-100'
+							onClick={ev => this.close.emit(ev)}
+						>
+							<p-icon variant='negative' />
+						</div>
+					)}
+				</div>
 			</Host>
 		);
+	}
+
+	@Listen('localeChanged', { target: 'body' })
+	private async _setLocales(): Promise<void> {
+		this._locales = await getLocaleComponentStrings(this._el);
 	}
 }
--- packages/core/src/components/atoms/floating-menu-item/floating-menu-item.component.tsx
+++ packages/core/src/components/atoms/floating-menu-item/floating-menu-item.component.tsx
@@ -1,9 +1,58 @@
 import { Component, h, Host, Prop } from '@stencil/core';
+import { IconFlipOptions, IconVariant } from '../icon/icon.component';
+import { RotateOptions } from '../../../types';
+import { cva } from 'class-variance-authority';
+
+const item = cva(
+	[
+		'p-floating-menu-item group',
+		'flex gap-2 items-center h-6',
+		'text-sm text-dark-teal-300',
+	],
+	{
+		variants: {
+			hover: {
+				false: null,
+				true: null,
+			},
+			disabled: {
+				false: null,
+				true: 'opacity-60 cursor-not-allowed',
+			},
+		},
+		compoundVariants: [
+			{
+				disabled: false,
+				hover: true,
+				class: 'cursor-pointer hover:text-dark-teal-100',
+			},
+		],
+	}
+);
+
+const icon = cva(['text-dark-teal-200'], {
+	variants: {
+		hover: {
+			false: null,
+			true: null,
+		},
+		disabled: {
+			false: null,
+			true: null,
+		},
+	},
+	compoundVariants: [
+		{
+			disabled: false,
+			hover: true,
+			class: 'group-hover:text-dark-teal-100',
+		},
+	],
+});
 
 @Component({
 	tag: 'p-floating-menu-item',
-	styleUrl: 'floating-menu-item.component.scss',
-	shadow: true,
+	styleUrl: 'floating-menu-item.component.css',
 })
 export class FloatingMenuItem {
 	/**
@@ -16,11 +65,67 @@ export class FloatingMenuItem {
 	 */
 	@Prop({ reflect: true }) disabled: boolean = false;
 
+	/**
+	 * Wether loading is applied
+	 */
+	@Prop() loading: boolean = false;
+
+	/**
+	 * Icon to show on the item
+	 */
+	@Prop() icon: IconVariant;
+
+	/**
+	 * Icon position
+	 */
+	@Prop() iconPosition: 'start' | 'end' = 'end';
+
+	/**
+	 * Icon flip
+	 */
+	@Prop() iconFlip: IconFlipOptions;
+
+	/**
+	 * Icon rotate
+	 */
+	@Prop() iconRotate: RotateOptions;
+
 	render() {
 		return (
-			<Host class="p-floating-menu-item">
-				<slot />
+			<Host
+				class={item({
+					hover: this.hover && !this.loading,
+					disabled: this.disabled,
+				})}
+			>
+				{this._getIcon()}
+
+				<p class='min-w-0 overflow-hidden text-ellipsis text-nowrap font-semibold'>
+					<slot />
+				</p>
 			</Host>
+		);
+	}
+
+	private _getIcon() {
+		if (!this.icon && !this.loading) {
+			return;
+		}
+
+		if (this.loading) {
+			return <p-loader color='white' />;
+		}
+
+		return (
+			<p-icon
+				class={icon({
+					hover: this.hover,
+					disabled: this.disabled,
+				})}
+				variant={this.icon}
+				flip={this.iconFlip}
+				rotate={this.iconRotate}
+			/>
 		);
 	}
 }
