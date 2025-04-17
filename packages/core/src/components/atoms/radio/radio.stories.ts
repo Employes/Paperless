afbcfae32942193ab88ff065ab97eeab56a7c372
Jurien Hamaker <whoami@jurien.dev>
2025-04-17 23:26:05 +0200

    feat(atoms/radio): Update story to contain label & apply nonce if id length is null

--- packages/core/src/components/atoms/radio/radio.stories.ts
+++ packages/core/src/components/atoms/radio/radio.stories.ts
@@ -1,10 +1,42 @@
+import { html } from 'lit';
+
 const meta = {
 	title: 'Design System/Atoms/Radio',
 	component: 'p-radio',
+	args: {
+		content: 'Label',
+	},
+	argTypes: {
+		content: {
+			control: {
+				type: 'text',
+			},
+		},
+	},
 };
 
 export default meta;
 
 export const Default = {
+	render: ({
+		content,
+		checked,
+		disabled,
+		id,
+		name,
+		required,
+		value,
+		checkedChange,
+	}) => html` <p-radio
+		checked=${checked}
+		disabled=${disabled}
+		id=${id}
+		name=${name}
+		required=${required}
+		value=${value}
+		@checkedChange=${checkedChange}
+	>
+		${content}
+	</p-radio>`,
 	tags: ['!dev'],
 };
