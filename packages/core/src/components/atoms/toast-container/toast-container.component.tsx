27c58bb60cab565cb46ede753dae3cbd72409023
Jurien Hamaker <whoami@jurien.dev>
2025-05-01 10:50:22 +0200

    refactor(atoms/toast-container): Rebranding applied

---
+++ packages/core/src/components/atoms/toast-container/toast-container.component.tsx
@@ -0,0 +1,60 @@
+import { Component, h, Prop } from '@stencil/core';
+import { cva } from 'class-variance-authority';
+
+const container = cva(
+	[
+		'fixed flex flex-col gap-4 z-toast',
+		'tablet:max-w-[26.5rem]',
+		'z-[600]',
+
+		'w-[calc(100%-(env(safe-area-inset-left,0)+env(safe-area-inset-right, 0)+2rem))]',
+		'max-w-[calc(100%-(env(safe-area-inset-left,0)+env(safe-area-inset-right,0)+2rem))]',
+	],
+	{
+		variants: {
+			placement: {
+				'top-start': [
+					'top-[calc(env(safe-area-inset-top,0)+1rem)]',
+					'left-[calc(env(safe-area-inset-left,0)+1rem)]',
+				],
+				'top-end': [
+					'top-[calc(env(safe-area-inset-top,0)+1rem)]',
+					'right-[calc(env(safe-area-inset-right,0)+1rem)]',
+				],
+				'bottom-start': [
+					'bottom-[calc(env(safe-area-inset-bottom,0)+1rem)]',
+					'left-[calc(env(safe-area-inset-left,0)+1rem)]',
+				],
+				'bottom-end': [
+					'bottom-[calc(env(safe-area-inset-bottom,0)+1rem)]',
+					'right-[calc(env(safe-area-inset-right,0)+1rem)]',
+				],
+			},
+		},
+	}
+);
+
+@Component({
+	tag: 'p-toast-container',
+	styleUrl: 'toast-container.component.css',
+	shadow: true,
+})
+export class ToastContainer {
+	/**
+	 * The placement of the container
+	 */
+	@Prop() placement: 'top-start' | 'top-end' | 'bottom-start' | 'bottom-end' =
+		'top-end';
+
+	render() {
+		return (
+			<div
+				class={container({
+					placement: this.placement,
+				})}
+			>
+				<slot />
+			</div>
+		);
+	}
+}
