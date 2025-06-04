de207501bd34452bbac88ffb11ce43ca4269f2ac
Bart van Zanten <bart@employes.nl>
2025-06-04 13:14:27 +0200

    fix(molecules/profile): Set subtitle text color to black-teal-400

--- packages/core/src/components/molecules/profile/profile.component.tsx
+++ packages/core/src/components/molecules/profile/profile.component.tsx
@@ -97,7 +97,7 @@ export class Profile {
 					<p class='my-0 w-auto max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm font-medium'>
 						<slot name='title' />
 					</p>
-					<p class='my-0 w-auto max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-xs text-black-teal-300'>
+					<p class='my-0 w-auto max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-xs text-black-teal-400'>
 						<slot name='subtitle' />
 					</p>
 				</div>
