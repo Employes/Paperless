db263c3f7766c6cc51fe9506c0f656a11d41ae46
Jurien Hamaker <whoami@jurien.dev>
2025-07-03 15:41:07 +0200

    refactor(atoms/badge): Set min-width to 1 rem

--- packages/core/src/components/atoms/badge/badge.component.tsx
+++ packages/core/src/components/atoms/badge/badge.component.tsx
@@ -3,7 +3,7 @@ import { cva } from 'class-variance-authority';
 
 const badge = cva([
 	'inline-flex items-center justify-center',
-	'h-4 min-w-[1.5rem] px-1',
+	'h-4 min-w-4 px-1',
 	'rounded-full',
 	'bg-dark-teal-400',
 	'text-center text-xs font-semibold text-white tracking-tight',
