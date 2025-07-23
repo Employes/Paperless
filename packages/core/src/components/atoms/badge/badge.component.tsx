e866b6ffa50b62aa17aa51c15620288f90d3c059
Jurien Hamaker <whoami@jurien.dev>
2025-07-23 12:23:37 +0200

    refactor(atoms/badge): Apply new colors

--- packages/core/src/components/atoms/badge/badge.component.tsx
+++ packages/core/src/components/atoms/badge/badge.component.tsx
@@ -5,8 +5,8 @@ const badge = cva([
 	'inline-flex items-center justify-center',
 	'h-4 min-w-4 px-1',
 	'rounded-full',
-	'bg-dark-teal-400',
-	'text-center text-xs font-semibold text-white tracking-tight',
+	'bg-storm-100',
+	'text-center text-xs font-semibold text-storm tracking-tight',
 ]);
 
 @Component({
