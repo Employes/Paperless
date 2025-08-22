e37c1c005a77ca96b79f4daf83e04e3e2bad8a8a
Jurien Hamaker <jurien@employes.nl>
2025-08-22 13:32:28 +0200

    refactor(atom/radio): Apply dark mode variant

--- packages/core/src/components/atoms/radio/radio.component.tsx
+++ packages/core/src/components/atoms/radio/radio.component.tsx
@@ -10,6 +10,7 @@ import {
 import { cva } from 'class-variance-authority';
 import { asBoolean } from '../../../utils/as-boolean';
 import { nonce } from '../../../utils/nonce';
+import { ThemedHost } from '../../../internal/themed-host.component';
 
 const radio = cva(
 	[
@@ -22,16 +23,16 @@ const radio = cva(
 			disabled: {
 				false: [
 					'cursor-pointer shadow-1',
-					'bg-white border-storm-100',
-					'hover:bg-indigo-100 hover:border-storm-500/20',
+					'bg-white border-storm-100 dark:bg-white/10 dark:border-white/15',
+					'hover:bg-indigo-100 hover:border-storm-500/20 dark:hover:bg-white/20 dark:hover:border-white/15',
 					'group-hover/radio-label:bg-indigo-100 group-hover/radio-label:border-storm-500/20',
 					'checked:cursor-auto',
 					'checked:bg-indigo-600 checked:border-storm-500/20',
-					'checked:ring-2 checked:ring-indigo-100',
+					'checked:ring-2 checked:ring-indigo-100 dark:checked:ring-0',
 					'checked:hover:bg-indigo-700 checked:hover:border-storm-500/20',
 					'checked:group-hover/radio-label:bg-indigo-700 checked:group-hover/radio-label:border-storm-500/20',
 				],
-				true: 'bg-white-600 border-storm-50 cursor-not-allowed ',
+				true: 'bg-white-600 border-storm-50 cursor-not-allowed dark:bg-white/5 dark:border-white/10',
 			},
 		},
 		compoundVariants: [
@@ -60,7 +61,7 @@ const circle = cva(
 			disabled: {
 				false:
 					'peer-checked:bg-white peer-checked:ring-1 peer-checked:ring-storm-500/10',
-				true: 'peer-checked:bg-storm-200',
+				true: 'peer-checked:bg-storm-200 dark:peer-checked:bg-hurricane-300',
 			},
 		},
 	}
@@ -127,34 +128,36 @@ export class Radio {
 		const id = this.id?.length ? this.id : this._nonce;
 
 		return (
-			<label
-				htmlFor={id}
-				class='flex items-center justify-start gap-2'
-			>
-				<div class='relative flex flex-shrink-0 items-center'>
-					<input
-						class={radio({
-							disabled: asBoolean(this.disabled),
-						})}
-						type='radio'
-						id={id}
-						name={this.name}
-						required={this.required}
-						value={this.value}
-						checked={this.checked}
-						disabled={asBoolean(this.disabled)}
-						onChange={ev => this._onChange(ev)}
-					/>
-					<div
-						class={circle({
-							disabled: asBoolean(this.disabled),
-						})}
-					/>
-				</div>
-				<div class='flex-1 overflow-hidden text-ellipsis'>
-					<slot />
-				</div>
-			</label>
+			<ThemedHost>
+				<label
+					htmlFor={id}
+					class='flex items-center justify-start gap-2'
+				>
+					<div class='relative flex flex-shrink-0 items-center'>
+						<input
+							class={radio({
+								disabled: asBoolean(this.disabled),
+							})}
+							type='radio'
+							id={id}
+							name={this.name}
+							required={this.required}
+							value={this.value}
+							checked={this.checked}
+							disabled={asBoolean(this.disabled)}
+							onChange={ev => this._onChange(ev)}
+						/>
+						<div
+							class={circle({
+								disabled: asBoolean(this.disabled),
+							})}
+						/>
+					</div>
+					<div class='flex-1 overflow-hidden text-ellipsis dark:text-white'>
+						<slot />
+					</div>
+				</label>
+			</ThemedHost>
 		);
 	}
 
