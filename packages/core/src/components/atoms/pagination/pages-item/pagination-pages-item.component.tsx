66a93941c4c15b18d966a222ee587836aef8cc6f
Jurien Hamaker <jurien@employes.nl>
2025-09-02 17:28:11 +0200

    refactor(atoms/pagination-pages-item): Apply dark mode variant

--- packages/core/src/components/atoms/pagination/pages-item/pagination-pages-item.component.tsx
+++ packages/core/src/components/atoms/pagination/pages-item/pagination-pages-item.component.tsx
@@ -1,6 +1,7 @@
 import { Component, h, Prop } from '@stencil/core';
 import { cva } from 'class-variance-authority';
 import { asBoolean } from '../../../../utils/as-boolean';
+import { ThemedHost } from '../../../../internal/themed-host.component';
 
 const item = cva(
 	[
@@ -12,7 +13,7 @@ const item = cva(
 	{
 		variants: {
 			variant: {
-				default: 'rounded text-storm-500',
+				default: 'rounded',
 				carousel: 'rounded-full border border-solid',
 			},
 			active: {
@@ -32,36 +33,47 @@ const item = cva(
 			{
 				variant: 'default',
 				active: false,
-				class: 'bg-off-white-300',
+				class:
+					'bg-off-white-300 dark:bg-hurricane-500 text-storm-400 dark:text-hurricane-200',
 			},
 			{
 				variant: 'default',
 				active: false,
 				hover: true,
-				class: 'hover:bg-off-white-500 cursor-pointer',
+				class:
+					'hover:text-storm-500 hover:bg-off-white-500 dark:hover:bg-white/15 dark:hover:text-hurricane-100 cursor-pointer',
 			},
 			{
 				variant: 'default',
 				active: true,
-				class: 'bg-off-white-700 font-semibold',
+				class:
+					'text-storm-500 bg-off-white-700 dark:bg-hurricane-300 dark:text-white font-semibold',
 			},
 
 			{
 				variant: 'carousel',
 				disabled: false,
-				class: 'bg-white text-storm-300 border-storm-100',
+				class: [
+					'bg-white text-storm-300 border-storm-100',
+					'dark:bg-white/15 dark:border-white/15 dark:text-hurricane-200',
+				],
 			},
 			{
 				variant: 'carousel',
 				disabled: false,
 				hover: true,
-				class:
+				class: [
 					'hover:bg-white-600 hover:text-storm-500 active:bg-white-600 active:border-indigo-600 active:ring active:ring-2 active:ring-indigo-100 cursor-pointer',
+					'dark:hover:ring-0 dark:hover:bg-white/25 dark:hover:text-white',
+				],
 			},
 			{
 				variant: 'carousel',
 				disabled: true,
-				class: 'bg-white-600 text-storm-100 border-storm-500/20',
+				class: [
+					'bg-white-600 text-storm-100 border-storm-500/20',
+					'dark:bg-hurricane-500 dark:border-white/5 dark:text-hurricane-300',
+				],
 			},
 		],
 	}
@@ -95,16 +107,18 @@ export class PaginationPagesItem {
 
 	render() {
 		return (
-			<div
-				class={item({
-					variant: this.variant ?? 'default',
-					active: asBoolean(this.active),
-					hover: asBoolean(this.hover, true),
-					disabled: asBoolean(this.disabled),
-				})}
-			>
-				<slot />
-			</div>
+			<ThemedHost>
+				<div
+					class={item({
+						variant: this.variant ?? 'default',
+						active: asBoolean(this.active),
+						hover: asBoolean(this.hover, true),
+						disabled: asBoolean(this.disabled),
+					})}
+				>
+					<slot />
+				</div>
+			</ThemedHost>
 		);
 	}
 }
