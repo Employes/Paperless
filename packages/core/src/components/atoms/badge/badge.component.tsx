3dfef9ede04d42ad0b7135d14828026418939a11
Jurien Hamaker <jurien@employes.nl>
2025-09-02 17:08:40 +0200

    refactor(atoms/badge): Apply dark mode variant

--- packages/core/src/components/atoms/badge/badge.component.tsx
+++ packages/core/src/components/atoms/badge/badge.component.tsx
@@ -1,12 +1,14 @@
 import { Component, h } from '@stencil/core';
 import { cva } from 'class-variance-authority';
+import { ThemedHost } from '../../../internal/themed-host.component';
 
 const badge = cva([
 	'inline-flex items-center justify-center',
 	'h-4 min-w-4 px-1',
 	'rounded-full',
-	'bg-storm-100',
-	'text-center text-xs font-semibold text-storm-500 tracking-tight',
+	'bg-storm-100 dark:bg-white/20',
+	'text-storm-500 dark:text-white',
+	'text-center text-xs font-semibold tracking-tight',
 ]);
 
 @Component({
@@ -17,9 +19,11 @@ const badge = cva([
 export class Badge {
 	render() {
 		return (
-			<div class={badge()}>
-				<slot />
-			</div>
+			<ThemedHost>
+				<div class={badge()}>
+					<slot />
+				</div>
+			</ThemedHost>
 		);
 	}
 }
