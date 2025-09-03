3f30b0ffcd740172db7a49488cdfe4e10d0b60b6
Jurien Hamaker <jurien@employes.nl>
2025-09-03 11:01:32 +0200

    refactor(molecules/empty-state): Apply dark mode variant

--- packages/core/src/components/molecules/empty-state/empty-state.component.tsx
+++ packages/core/src/components/molecules/empty-state/empty-state.component.tsx
@@ -13,6 +13,7 @@ import {
 	IllustrationVariant,
 } from '../../../components';
 import { asBoolean } from '../../../utils/as-boolean';
+import { ThemedHost } from '../../../internal/themed-host.component';
 
 const emptyState = cva(
 	['flex flex-col w-full items-center justify-start max-w-[20rem]'],
@@ -81,8 +82,9 @@ export class EmptyState {
 	 * The text on the action button
 	 */
 	@Event({
-		bubbles: false
-	}) action: EventEmitter<MouseEvent>;
+		bubbles: false,
+	})
+	action: EventEmitter<MouseEvent>;
 
 	/**
 	 * The host element
@@ -97,55 +99,57 @@ export class EmptyState {
 		const hasActionSlot = !!this._el.querySelector(':scope > [slot="action"]');
 
 		return (
-			<div
-				class={emptyState({
-					enableAction: asBoolean(this.enableAction, true),
-				})}
-				onClick={ev =>
-					this.enableAction && !this.actionLoading
-						? this.action.emit(ev)
-						: undefined
-				}
-			>
-				{this.illustration && (
-					<p-illustration
-						class='mb-8 text-5xl text-indigo-600'
-						variant={this.illustration}
-					/>
-				)}
-				{hasHeaderSlot ? (
-					<slot name='header' />
-				) : (
-					this.header?.length && (
-						<p class='my-0 text-center font-ambit text-sm font-bold text-storm-500'>
-							{this.header}
-						</p>
-					)
-				)}
-				{hasContentSlot ? (
-					<slot name='content' />
-				) : (
-					this.content?.length && (
-						<p class='my-0 text-center text-sm font-normal text-storm-300'>
-							{this.content}
-						</p>
-					)
-				)}
-				{this.enableAction &&
-					(hasActionSlot ? (
-						<slot name='action' />
+			<ThemedHost>
+				<div
+					class={emptyState({
+						enableAction: asBoolean(this.enableAction, true),
+					})}
+					onClick={ev =>
+						this.enableAction && !this.actionLoading
+							? this.action.emit(ev)
+							: undefined
+					}
+				>
+					{this.illustration && (
+						<p-illustration
+							class='mb-8 text-5xl text-indigo-600'
+							variant={this.illustration}
+						/>
+					)}
+					{hasHeaderSlot ? (
+						<slot name='header' />
 					) : (
-						<p-button
-							class='mt-4'
-							icon={this.actionIcon}
-							iconPosition={this.actionIconPosition}
-							loading={this.actionLoading}
-							variant={this.actionVariant}
-						>
-							{this.actionText}
-						</p-button>
-					))}
-			</div>
+						this.header?.length && (
+							<p class='my-0 text-center font-ambit text-sm font-bold text-storm-500 dark:text-white'>
+								{this.header}
+							</p>
+						)
+					)}
+					{hasContentSlot ? (
+						<slot name='content' />
+					) : (
+						this.content?.length && (
+							<p class='my-0 text-center text-sm font-normal text-storm-300 dark:text-hurricane-200'>
+								{this.content}
+							</p>
+						)
+					)}
+					{this.enableAction &&
+						(hasActionSlot ? (
+							<slot name='action' />
+						) : (
+							<p-button
+								class='mt-4'
+								icon={this.actionIcon}
+								iconPosition={this.actionIconPosition}
+								loading={this.actionLoading}
+								variant={this.actionVariant}
+							>
+								{this.actionText}
+							</p-button>
+						))}
+				</div>
+			</ThemedHost>
 		);
 	}
 }
