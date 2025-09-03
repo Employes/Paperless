740b45882beb599f6c36a670eff505232bc0e85f
Jurien Hamaker <jurien@employes.nl>
2025-09-03 13:36:56 +0200

    fix(angular): Set dark mode variants

--- packages/angular/projects/playground/tailwind.config.js
+++ packages/angular/projects/playground/tailwind.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 
 module.exports = {
 	important: true,
+	darkMode: [
+		'variant',
+		[
+			'&:where(:host([data-theme=dark]),:host([data-theme=dark]) *)',
+			'&:where([data-theme=dark],[data-theme=dark] *)',
+		],
+	],
 	content: [
 		path.join(__dirname, './src/**/*.{html,scss,ts}'),
 		path.join(__dirname, '../paperless/src/**/*.{html,scss,ts}'),
