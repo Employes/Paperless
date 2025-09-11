cd0a37cce49d82cacfb98ac12b296e45711ce5d6
Jurien Hamaker <jurien@employes.nl>
2025-09-11 10:42:05 +0200

    fix(atoms/modal/footer): Set default color for divider

--- packages/core/src/components/atoms/modal/footer/modal-footer.component.tsx
+++ packages/core/src/components/atoms/modal/footer/modal-footer.component.tsx
@@ -11,7 +11,7 @@ export class ModalFooter {
 		return (
 			<ThemedHost>
 				<div class='flex flex-col rounded-b-none bg-white px-4 pb-[calc(env(safe-area-inset-bottom)+1rem)] desktop-xs:rounded-b-2xl dark:bg-hurricane-600'>
-					<p-divider class='mb-4 hidden desktop-xs:inline-block dark:text-white/10' />
+					<p-divider class='mb-4 hidden text-off-white-700 desktop-xs:inline-block dark:text-white/10' />
 
 					<div class='flex flex-col gap-4 tablet:flex-row'>
 						<slot />
