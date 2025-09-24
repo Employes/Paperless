e0802a883d929eb8ae16b7f64f042b86f012ac50
Jim Overmeen <jim@employes.nl>
2025-09-24 15:09:48 +0200

    fix(atoms/tab/container): deleted z-index

--- packages/core/src/components/atoms/tab/container/tab-container.component.tsx
+++ packages/core/src/components/atoms/tab/container/tab-container.component.tsx
@@ -9,7 +9,7 @@ export class TabContainer {
 	render() {
 		return (
 			<div class='relative flex h-8 w-full flex-col'>
-				<div class='scrollbar-hide z-10 flex w-full gap-5 overflow-x-auto'>
+				<div class='scrollbar-hide flex w-full gap-5 overflow-x-auto'>
 					<slot />
 				</div>
 				<p-divider class='absolute bottom-0 left-0 m-0 w-full text-off-white-700 dark:text-hurricane-400' />
