beb4bd283e3d1cf0455863bd5f7d668795594fef
Jurien Hamaker <jurien@employes.nl>
2026-01-14 16:42:16 +0100

    fix(state): Export more information

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,18 @@
 export { Components, JSX } from './components';
 
-export { state, onStateChange, initTheme } from './state';
+export {
+	state,
+	onStateChange,
+	initTheme,
+	stateLocaleOptions,
+	stateThemeOptions,
+} from './state';
+
+export type {
+	StateThemeOption,
+	StateLocaleOption,
+	PaperlessState,
+} from './state';
 
 export { paperless } from './tailwind';
 export * from './types';
