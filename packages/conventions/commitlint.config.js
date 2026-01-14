88494e49715dd52f2956509a395883d6679f9835
Jurien Hamaker <jurien@employes.nl>
2026-01-14 09:07:11 +0100

    refactor(conventions): Remove commitlint config

--- packages/conventions/commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	extends: ['@commitlint/config-conventional'],
-	rules: {
-		'header-max-length': [0, 'always'],
-		'subject-case': [2, 'never', ['pascal-case', 'upper-case']],
-		'scope-case': [2, 'always', 'kebab-case'],
-	},
-};
