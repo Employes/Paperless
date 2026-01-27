0a85d5c073071ee2260b6147b0bc4ca571169e3a
Jurien Hamaker <whoami@jurien.dev>
2026-01-27 18:01:12 +0100

    feat(angular): Move defineCustomElements to module

--- packages/angular/projects/paperless/src/lib/paperless.module.ts
+++ packages/angular/projects/paperless/src/lib/paperless.module.ts
@@ -1,16 +1,21 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, provideAppInitializer } from '@angular/core';
 
 import { DIRECTIVES } from './directives';
 import { MODULES } from './modules';
 import { PIPES } from './pipes';
 import { DIRECTIVES as STENCIL_DIRECTIVES } from './stencil';
+import { defineCustomElements } from '@paperless/core/loader';
 
 const NGX_PIPES = [DatePipe, CurrencyPipe];
 
 @NgModule({
 	imports: [...STENCIL_DIRECTIVES, ...DIRECTIVES, ...MODULES, ...PIPES],
 	exports: [...MODULES, ...DIRECTIVES, ...STENCIL_DIRECTIVES, ...PIPES],
-	providers: [...NGX_PIPES, ...PIPES],
+	providers: [
+		...NGX_PIPES,
+		...PIPES,
+		provideAppInitializer(() => defineCustomElements()),
+	],
 })
 export class PaperlessModule {}
