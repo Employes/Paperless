6f6078cf51097200a959de0c97889a01fff43a9f
Jurien Hamaker <whoami@jurien.dev>
2026-02-03 10:19:36 +0100

    fix(molecules/table/footer): Move pointer-events-none to host

--- packages/core/src/components/molecules/table/footer/table-footer.component.tsx
+++ packages/core/src/components/molecules/table/footer/table-footer.component.tsx
@@ -20,7 +20,6 @@ const footer = cva(
 	[
 		'justify-center relative',
 		'w-full px-6',
-		'pointer-events-none',
 		'after:absolute after:top-0 after:left-0 after:z-[0]',
 		'after:w-full after:h-full after:transition-opacity',
 		'after:bg-gradient-to-t after:from-white dark:after:from-hurricane-700 after:via-white/80 dark:after:via-hurricane-700/80 after:to-transparent',
